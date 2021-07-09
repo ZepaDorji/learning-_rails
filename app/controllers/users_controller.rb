eb93a38d4c8b0b7536b3ed599ec4c16e301eb89a
Zepa Dorji <zepadorji222@gmail.com>
2021-07-09 23:08:35 +0600

    add page number in views of

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.paginate(page: params[:page], per_page: 3)
+    end
     
     def new
         @user = User.new
@@ -6,7 +9,7 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
     def edit
@@ -17,7 +20,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your information is succssfully updated"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
