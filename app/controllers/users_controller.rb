1790a1a99a91e9d0517b2a060c55f0c88bd52919
Zepa Dorji <zepadorji222@gmail.com>
2021-07-09 20:55:27 +0600

    added user profile with image using gravatar from wordpress and render its related article in show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
+    
     def new
         @user = User.new
     end
+
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+
     def edit
         @user = User.find(params[:id])
     end
+
     def update
         @user = User.find(params[:id])
         if @user.update(user_params)
@@ -14,6 +22,7 @@ class UsersController < ApplicationController
             render 'edit'
         end
     end
+
     def create 
         @user = User.new(user_params)
         if @user.save
