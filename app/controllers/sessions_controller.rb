b3e7c22e0a9505120bae60a68e54aaafd35a42f3
Zepa Dorji <zepadorji222@gmail.com>
2021-07-10 00:18:52 +0600

    completed users create and destroy action in controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,20 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(email: params[:session][:email].downcase) # this seach for the user exit in db
+        if user && user.authenticate(params[:session][:password]) # if user found and password matched
+            session[:user_id] = user.id
+            flash[:notice] = "Log In successfull"
+            redirect_to user
+        else
+            flash.now[:alert] = "Login Failed"
+            render 'new'
+        end
     end
 
     def destroy
+        session[:user_id] = nil
+        flash[:notice] = "You log out successfully"
+        redirect_to root_path
     end
 end
