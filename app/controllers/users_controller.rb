97c161d7559a5551776ad627e84b5e36f5568280
Zepa Dorji <zepadorji222@gmail.com>
2021-07-10 10:28:05 +0600

    add current user and log out in helper application and moifited the nav bar and user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+    before_action :set_user, only:[:show, :edit, :update]
+
     def index
         @users = User.paginate(page: params[:page], per_page: 3)
     end
@@ -8,16 +11,16 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
+        #@user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
     def edit
-        @user = User.find(params[:id])
+        #@user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
+        #@user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your information is succssfully updated"
             redirect_to @user
@@ -29,6 +32,7 @@ class UsersController < ApplicationController
     def create 
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "WELLCOME TO BLOG #{@user.username}, you have succssfully sing up "
             redirect_to articles_path
         else
@@ -39,5 +43,8 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+    def set_user
+        @user = User.find(params[:id])
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,20 @@
 module ApplicationHelper
+  #for user image 
     def gravatar_for(user, options = { size: 80})
         email_address = user.email.downcase
         hash = Digest::MD5.hexdigest(email_address)
         size = options[:size]
         gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
         image_tag(gravatar_url, alt: user.username)
+      end
+
+      def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+        #if we have current user already it will not hit to db time and again.
+         
+      end
+
+      def logged_in?
+        !!current_user # coverting current user into boolean
       end
 end
