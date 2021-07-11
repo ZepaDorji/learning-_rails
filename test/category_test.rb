96b3cbc6593f9026a0cd313bdfe93ad96c623110
Zepa Dorji <zepadorji222@gmail.com>
2021-07-11 12:50:12 +0600

    creates category model, tested and unit test- validations for category

---
+++ test/category_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+class CategoryTest < ActiveSupport::TestCase
+    def setup # runs before any following tests
+        @category = Category.new(name: "Sports") # making this instant as global 
+    end
+    test "category should be valid" do 
+        #@category = Category.new(name: "Sports")
+        assert @category.valid?  # assert check for true
+    end
+    test "name should be present" do 
+        @category.name = ""
+        assert_not @category.valid?  # assert_not check for false
+    end
+    test "name should be unique" do
+        @category.save  # for uniquness, atlest one data should present in db, so we are saving sport
+        @category2 = Category.new(name: "Sports")  # creating category with same name
+        assert_not @category2.valid?
+    end
+    test "name should not be too long" do
+        @category.name = "Z"*26
+        assert_not @category.valid?
+    end
+    test "name should not be too short" do 
+        @category.name = "zz"
+        assert_not @category.valid?
+    end
+end
